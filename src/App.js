9480b478caf6dc073446f96616761db2e3c66815
damico williams <damicowilliams@gmail.com>
2020-02-18 16:35:38 -0800

    fixed avatar error

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import Githubcard from './components/Githubcard.js'
+import Followers from './components/Followers.js'
+import { Grid } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      
+      userData: [],
+      userFollowers: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`https://api.github.com/users/damico-williams`)
+    
+    .then(res => {
+      this.setState({ userData: res.data })
+      console.log(res.data)
+    })
+    .catch(error => {
+      console.log(error)
+    })
+
+    axios.get(`https://api.github.com/users/${this.state.user}/followers`)
+    
+    .then(res => {
+      this.setState({ userFollowers: res.data })
+      })
+      .catch(error => {
+        console.log(error)
+    })
+  }
+
+  render() {
+    return(
+      <div className="App">
+         <Grid celled>
+           <Grid.Row verticalAlign='top'>
+             <Grid.Column textAlign='center' width={4}>
+                <h2>User</h2>
+                <Githubcard userData={this.state.userData}/>
+             </Grid.Column>
+             <Grid.Column textAlign='center' width={12}>
+                <h2>Friends</h2>
+                <Followers followers={this.state.userFollowers} />
+             </Grid.Column>
+           </Grid.Row>
+        </Grid>
+      </div>
+    )
+  }
 }
 
 export default App;
